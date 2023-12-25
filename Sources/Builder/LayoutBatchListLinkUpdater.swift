a936dc19f4b5be5182741b73d86d6933acfe26b6
huangwenfei <yi.yuan.zi@163.com>
2023-12-25 14:57:07 +0800

    - 新增 BatchList 未实现

---
+++ Sources/Builder/LayoutBatchListLinkUpdater.swift
@@ -0,0 +1,8 @@
+//
+//  LayoutBatchListLinkUpdater.swift
+//  Yang
+//
+//  Created by windy on 2023/12/25.
+//
+
+import Foundation
---
+++ Sources/Manager/LayoutBatchListManager.swift
@@ -0,0 +1,8 @@
+//
+//  LayoutBatchListManager.swift
+//  Yang
+//
+//  Created by windy on 2023/12/25.
+//
+
+import Foundation
